6ac9f217358e50cad9c46ef63ee88693eb41cf94
Marwan Mohamed Fahmy <90521144+Maro7772@users.noreply.github.com>
2025-07-02 23:05:06 +0300

    Add files via upload

---
+++ routes/taskRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { protect, adminOnly } from "../middlewares/authMiddleware.js";
+import {
+  createTask,
+  getTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+  updateTaskStatus,
+  updateTaskCheckList,
+  getDashboardData,
+  getUserDashboardData
+} from "../controllers/taskController.js";
+
+const router = express.Router();
+
+router.get("/dashboard-data", protect, getDashboardData);
+router.get("/user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks);
+router.get("/:id", protect, getTaskById);
+router.post("/", protect, adminOnly, createTask);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, adminOnly, deleteTask);
+router.put("/:id/status", protect, updateTaskStatus);
+router.put("/:id/todo", protect, updateTaskCheckList);
+
+export default router;
